3f5c6a07cad29d17942c556cc3c26f233a359c49
Ma'murjon <mamurjon@MacBook-Air-Mamurjon.local>
2022-05-14 10:13:18 +0500

    s

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Home from "./Home";
 import Products from "./Products";
+// import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 const App = function () {
   return (
     <div>
       <Header />
-      <Home />
-      <Products />
+      <Routes>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={Products} />
+      </Routes>
     </div>
   );
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,28 +1,20 @@
 import React, { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
 
 const Products = function () {
-  // const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
   const [filter, setFilter] = useState([]);
   const [loading, setLoading] = useState(true);
-  let componentMounted = true;
   useEffect(() => {
     setLoading(true);
-
     const getProducts = async () => {
       const response = await fetch("https://fakestoreapi.com/products");
-      if (componentMounted) {
-        setFilter(await response.json());
-        console.log(filter);
-        setLoading(false);
-      }
-      return () => {
-        componentMounted = false;
-      };
-      // componentMounted = false;
+      setData(await response.clone().json());
+      setFilter(await response.json());
+      setLoading(false);
     };
-    // getProducts();
-    console.log(componentMounted);
-  }, [filter]);
+    getProducts();
+  }, []);
   const Loading = function () {
     return (
       <div>
@@ -30,14 +22,150 @@ const Products = function () {
       </div>
     );
   };
-  // const readyProducts = function () {};
+  const ReadyProducts = function () {
+    return filter.map((val, a) => {
+      return (
+        <div
+          key={a}
+          className="ui link cards"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <div className="card">
+            <div className="image">
+              <img src={val.image} style={{ width: "100%", height: "300px" }} />
+            </div>
+            <div className="extra content">
+              <div className="right floated">{val.price}$</div>
+              <span>{val.title}</span>
+            </div>
+            <div style={{ textAlign: "center", paddingBottom: "1rem" }}>
+              <div
+                className="ui horizontal animated button"
+                tabindex="0"
+                style={{ width: "50%" }}
+              >
+                <div className="hidden content">Buy Now</div>
+                <div className="visible content">
+                  <i className="shop icon"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+  const filterProduct = (key) => {
+    const obj = data.filter((val) => {
+      return val.category == key;
+    });
+    setFilter(obj);
+  };
+  console.log(data);
+  const Categories = function () {
+    return (
+      <div
+        style={{ display: "flex", justifyContent: "center", padding: "2rem" }}
+      >
+        <div
+          class="ui animated fade button"
+          tabindex="0"
+          onClick={() => {
+            setFilter(data);
+          }}
+        >
+          <div class="visible content">All</div>
+          <div class="hidden content">
+            <i
+              class="hand point up outline icon"
+              style={{ fontSize: "1.5rem" }}
+            ></i>
+          </div>
+        </div>
+        <div
+          class="ui animated fade button"
+          tabindex="0"
+          onClick={() => {
+            filterProduct("men's clothing");
+          }}
+        >
+          <div class="visible content">Men's Clothing</div>
+          <div class="hidden content">
+            <i
+              class="hand point up outline icon"
+              style={{ fontSize: "1.5rem" }}
+            ></i>
+          </div>
+        </div>
+        <div
+          class="ui animated fade button"
+          tabindex="0"
+          onClick={() => {
+            filterProduct("women's clothing");
+          }}
+        >
+          <div class="visible content">Woman's Clothing</div>
+          <div class="hidden content">
+            <i
+              class="hand point up outline icon"
+              style={{ fontSize: "1.5rem" }}
+            ></i>
+          </div>
+        </div>
+        <div
+          class="ui animated fade button"
+          tabindex="0"
+          onClick={() => {
+            filterProduct("jewelery");
+          }}
+        >
+          <div class="visible content">Jewelery</div>
+          <div class="hidden content">
+            <i
+              class="hand point up outline icon"
+              style={{ fontSize: "1.5rem" }}
+            ></i>
+          </div>
+        </div>
+        <div
+          class="ui animated fade button"
+          tabindex="0"
+          onClick={() => {
+            filterProduct("electronics");
+          }}
+        >
+          <div class="visible content">Electronics</div>
+          <div class="hidden content">
+            <i
+              class="hand point up outline icon"
+              style={{ fontSize: "1.5rem" }}
+            ></i>
+          </div>
+        </div>
+      </div>
+    );
+  };
   return (
     <div>
       <div className="main-title">
         <h1>Latest Products</h1>
         <hr />
       </div>
-      <Loading />
+      {loading ? (
+        <Loading />
+      ) : (
+        <div>
+          <Categories />
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "1fr 1fr 1fr 1fr",
+            }}
+          >
+            <ReadyProducts />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
